7e2bed9f6c592183c921e6a364697465837a1839
dickson <kr.d.tsl@gmail.com>
2016-04-26 19:13:58 -0400

    fixed it all up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
-
   validates_presence_of :avatar
   validates_integrity_of :avatar
   validates_processing_of :avatar
