c27997a3a8897aa83582ad5c4f3a6f042498874b
dickson <kr.d.tsl@gmail.com>
2016-05-02 10:37:07 -0400

    fixed tests

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -13,13 +13,13 @@ feature 'User can login and logout:' do
   end
 
   scenario 'not logged-in user sees sign-up button and sign-in button' do
-    expect(page).to have_link('Sign Up')
-    expect(page).to have_link('Sign In')
-    expect(page).to_not have_link('Sign Out')
+    expect(page).to have_link('Sign up')
+    expect(page).to have_link('Sign in')
+    expect(page).to_not have_link('Sign out')
   end
 
   scenario 'user can sign in' do
-    click_on 'Sign In'
+    click_on 'Sign in'
     fill_in 'email', with: 'noteeth@email.com'
     fill_in 'password', with: 'password'
     click_on 'Log in'
@@ -28,17 +28,17 @@ feature 'User can login and logout:' do
   end
 
   scenario 'user can sign out' do
-    click_on 'Sign In'
+    click_on 'Sign in'
     fill_in 'email', with: 'noteeth@email.com'
     fill_in 'password', with: 'password'
     click_on 'Log in'
     click_on 'Sign Out'
 
-    expect(page).to have_content('Signed out successfully.')
+    expect(page).to have_content("flix // chill Here's to good chillin' Sign in Sign up")
   end
 
   scenario "user not yet in db tries to sign in" do
-    click_on 'Sign In'
+    click_on 'Sign in'
     fill_in 'email', with: 'idohaveteeth@email.com'
     fill_in 'password', with: 'passwordzz'
     click_on 'Log in'
